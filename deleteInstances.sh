ec8f884993841fc80537f90a93e7e50633ab4937
nadeem2019 <52539890+nadeem2019@users.noreply.github.com>
2019-10-23 00:28:38 +0530

    Update deleteInstances.sh

--- deleteInstances.sh
+++ deleteInstances.sh
@@ -2,7 +2,7 @@ delete () {
     gcloud -q compute instances delete $1
 }
 
-for i in master node-0 node-1 node-2; do
+for i in ndm_master ndm_node-0 ndm_node-1 ndm_node-2; do
     delete $i &
 done
 

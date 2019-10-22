1e289f9d08e7b87a80a8e72b2788e8bede7e1365
nadeem2019 <52539890+nadeem2019@users.noreply.github.com>
2019-10-23 00:25:17 +0530

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 gcloud config set compute/region us-central1
 gcloud config set compute/zone  us-central1-a
 
-gcloud compute instances create master --image-family ubuntu-1804-lts --image-project ubuntu-os-cloud --custom-cpu 2 --custom-memory 4
+gcloud compute instances create ndm_master --image-family ubuntu-1804-lts --image-project ubuntu-os-cloud --custom-cpu 2 --custom-memory 4
 
 createNodes() {
-    gcloud compute instances create node-$1 --image-family ubuntu-1804-lts --image-project ubuntu-os-cloud
+    gcloud compute instances create ndm_node-$1 --image-family ubuntu-1804-lts --image-project ubuntu-os-cloud
 }
 
 for i in {0..2}; do
@@ -19,7 +19,7 @@ allInstances() {
     cat installKubeadm.sh | gcloud compute ssh $1
 }
 
-for i in master node-0 node-1 node-2; do
+for i in ndm_master ndm_node-0 ndm_node-1 ndm_node-2; do
     allInstances $i &
 done
 
@@ -35,16 +35,16 @@ done
 
 wait
 
-cat installKubectl.sh | gcloud compute ssh master
-cat initCluster.sh | gcloud compute ssh master > initOutput.sh
-cat setupKubeconfig.sh | gcloud compute ssh master
-cat podNetworking.sh | gcloud compute ssh master
+cat installKubectl.sh | gcloud compute ssh ndm_master
+cat initCluster.sh | gcloud compute ssh ndm_master > initOutput.sh
+cat setupKubeconfig.sh | gcloud compute ssh ndm_master
+cat podNetworking.sh | gcloud compute ssh ndm_master
 
 echo 'sudo su' > joinNodes.sh
 cat initOutput.sh | tail -2 >> joinNodes.sh
 
 join() {
-    cat joinNodes.sh | gcloud compute ssh node-$1
+    cat joinNodes.sh | gcloud compute ssh ndm_node-$1
 }
 
 for i in {0..2}; do
